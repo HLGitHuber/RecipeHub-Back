99e1ec8a50455fb20821aa9be86d18e0b247ce4a
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-07-18 23:19:34 +0200

    feat: added IngredientsProfile.cs

---
+++ RecipeHub/Configuration/Mapper/IngredientsProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using RecipeHub.Domain;
+using RecipeHub.DTO_s;
+
+namespace RecipeHub.Configuration.Mapper;
+
+public class IngredientsProfile : Profile
+{
+    public IngredientsProfile()
+    {
+        CreateMap<Ingredient, IngredientDto>();
+        CreateMap<Ingredient, IngredientDetailsDto>();
+        CreateMap<IngredientForAddDto, Ingredient>();
+        CreateMap<IngredientForUpdateDto, Ingredient>();
+    }
+}
