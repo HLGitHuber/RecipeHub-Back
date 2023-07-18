9c3445a168154551d1c613db35c2968beda10562
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-07-18 23:19:21 +0200

    feat: added IngredientForUpdateDto.cs

---
+++ RecipeHub/DTO's/IngredientForUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeHub.DTO_s;
+
+public class IngredientForUpdateDto
+{
+    [Required]
+    [StringLength(128)]
+    public string Name { get; set; } = string.Empty;
+}
