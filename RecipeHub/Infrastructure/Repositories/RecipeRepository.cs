75101f47fc826e2bc8bb182808ddffb328f79a5c
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-08-18 16:28:09 +0200

    feat: added base of new repository

--- RecipeHub/Infrastructure/Repositories/RecipeRepository.cs
+++ RecipeHub/Infrastructure/Repositories/RecipeRepository.cs
@@ -1,6 +1,42 @@
-﻿namespace RecipeHub.Infrastructure.Repositories
+﻿using AutoMapper;
+using RecipeHub.Domain;
+
+namespace RecipeHub.Infrastructure.Repositories
 {
-    public class RecipeRepository
+    public class RecipeRepository : IRecipeRepository
     {
+        private readonly RecipeDBContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public RecipeRepository(RecipeDBContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+            _mapper = mapper;
+        }
+        
+        public IEnumerable<Recipe> GetRecipe(string? search)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Recipe? GetRecipe(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRecipe(Recipe recipe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateRecipe(Recipe recipe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteRecipe(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
