a537f38e88acf5f8cc053d86eb9b3964d07e5a8c
Adam Nowicki <adamnow83@gmail.com>
2023-08-26 13:52:26 +0200

    feat registration enpoint

--- RecipeHub/Controllers/UsersController.cs
+++ RecipeHub/Controllers/UsersController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using RecipeHub.Domain;
+using RecipeHub.DTO_s;
 
 namespace RecipeHub.Controllers
 {
@@ -8,12 +12,53 @@ namespace RecipeHub.Controllers
     [Authorize]
     public class UsersController: ControllerBase
     {
-        [HttpOptions("regiester")]
-        public Task<IActionResult> Register() 
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController
+            (UserManager<User> userManager,SignInManager<User> signInManager,
+            ILogger<UsersController> logger)
         {
-            throw new NotImplementedException();
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+            _signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
+
+        [HttpOptions("regiester")]
+        [ResponseCache(CacheProfileName = "NoCache")]
+        [AllowAnonymous]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Register([FromBody] UserForRegistrationDto userForRegistrationDto) 
+        {
+            var user = new User();
+            user.UserName = userForRegistrationDto.UserName;
+            user.Email = userForRegistrationDto.Email;
+
+            var result = await _userManager.CreateAsync(user, userForRegistrationDto.Password);
+            if(!result.Succeeded)
+            {
+                _logger.LogWarning($"User creation for {userForRegistrationDto.UserName} failed");
+                var errors = result.Errors.Select(e => e.Description);
+                return BadRequest(new {Errors = errors});
+            }
+            _logger.LogInformation($"User {userForRegistrationDto.UserName} created");
+            result = await _userManager.AddToRolesAsync(user, userForRegistrationDto.Roles);
+
+            if(!result.Succeeded)
+            {
+                _logger.LogWarning($"User {userForRegistrationDto.UserName} could not be assiged roles");
+                var errors = result.Errors.Select(e => e.Description);
+                return BadRequest(new { Errors = errors });
+            }
+            _logger.LogWarning($"User {userForRegistrationDto.UserName} assiged to roles");
+            return Accepted($"User {user.UserName} created");
+        }
+
         [HttpOptions("login-cookie")]
+        [ResponseCache(CacheProfileName = "NoCache")]
+        [AllowAnonymous]
         public Task<IActionResult> LoginCookie()
         {
             throw new NotImplementedException();
---
+++ RecipeHub/DTO's/UserForRegistrationDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeHub.DTO_s
+{
+    public class UserForRegistrationDto
+    {
+        [Required]
+        [MaxLength(256)]
+        public string USerName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        [MaxLength (256)]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(64)]
+        public string Password { get; set; } = string.Empty;
+        public ICollection<string> Roles { get; set; }= new List<string> ();
+    }
+}
