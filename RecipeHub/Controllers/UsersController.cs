6a4cbfa2be9cf56aa7afb9d789d07885d40fb9d8
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-09-01 17:40:52 +0200

    feat: added login with jwt

--- RecipeHub/Controllers/UsersController.cs
+++ RecipeHub/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
@@ -7,6 +8,10 @@ using Microsoft.AspNetCore.Mvc;
 using RecipeHub.Domain;
 using RecipeHub.DTO_s;
 using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using RecipeHub.Configuration.Options;
 
 namespace RecipeHub.Controllers
 {
@@ -18,14 +23,17 @@ namespace RecipeHub.Controllers
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
         private readonly ILogger<UsersController> _logger;
+        private readonly JwtConfiguration _jwtConfiguration;
 
         public UsersController
             (UserManager<User> userManager,SignInManager<User> signInManager,
-            ILogger<UsersController> logger)
+            ILogger<UsersController> logger,
+            IOptions<JwtConfiguration> jwtOptions)
         {
             _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
             _signInManager = signInManager ?? throw new ArgumentNullException(nameof(signInManager));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _jwtConfiguration = jwtOptions.Value ?? throw new ArgumentNullException(nameof(jwtOptions));
         }
 
         private IActionResult ReportErrors(IdentityResult result, User user)
@@ -128,6 +136,63 @@ namespace RecipeHub.Controllers
             return Accepted();
         }
         
+        
+        [HttpOptions("login-jwt")]
+    [ResponseCache(CacheProfileName = "NoCache")]
+    [AllowAnonymous]
+    [ProducesResponseType(StatusCodes.Status202Accepted)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> LoginJwt([FromBody] UserForLoginWithTokenDto userForLoginDto)
+    {
+        var user = await _userManager.FindByNameAsync(userForLoginDto.UserName);
+
+        if (user is null)
+        {
+            _logger.LogWarning("User {userName} not found.", userForLoginDto.UserName);
+
+            return Unauthorized(userForLoginDto);
+        }
+
+        var result = await _signInManager.CheckPasswordSignInAsync(user, userForLoginDto.Password, false);
+
+        if (!result.Succeeded)
+        {
+            _logger.LogWarning("User {userName} login failed.", userForLoginDto.UserName);
+
+            return Unauthorized(userForLoginDto);
+        }
+
+        _logger.LogInformation("User {userName} logged in.", userForLoginDto.UserName);
+
+        var claims = new List<Claim>();
+
+        claims.Add(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+        claims.Add(new Claim(ClaimTypes.Name, user.UserName!));
+
+        var roles = await _userManager.GetRolesAsync(user);
+
+        foreach (var role in roles)
+        {
+            claims.Add(new Claim(ClaimTypes.Role, role));
+        }
+
+        // Create JWT token
+
+        var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtConfiguration.SigningKey)), SecurityAlgorithms.HmacSha256);
+
+        var jwtObject = new JwtSecurityToken(
+            issuer: _jwtConfiguration.Issuer,
+            audience: _jwtConfiguration.Audience,
+            claims: claims,
+            expires: DateTime.Now.AddSeconds(300), // 5 minutes, tokens should be short-lived
+            signingCredentials: signingCredentials);
+
+        var tokenToReturn = new JwtSecurityTokenHandler().WriteToken(jwtObject);
+
+        _logger.LogInformation("User {userName} logged in.", userForLoginDto.UserName);
+
+        return Accepted(new { Token = tokenToReturn });
+    }
         
 
         [HttpOptions("access-denied")]
