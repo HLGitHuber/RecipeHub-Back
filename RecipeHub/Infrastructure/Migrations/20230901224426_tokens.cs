a0c11904ea44eaac7a2f88f9b6633fc3b30195e9
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-09-02 18:43:56 +0200

    feat: added JWT tokens

---
+++ RecipeHub/Infrastructure/Migrations/20230901224426_tokens.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace RecipeHub.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class tokens : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "78896574-7949-43c1-a17b-70c2b1a2e0f4");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "f3dada1c-f273-4657-b857-67d0334bad7a");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "31421b0b-1070-41e2-b523-cfdcb4ef79de", null, "User", "USER" },
+                    { "b15cdcd7-6750-49b2-a351-bd7615b0d95d", null, "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "31421b0b-1070-41e2-b523-cfdcb4ef79de");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "b15cdcd7-6750-49b2-a351-bd7615b0d95d");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "78896574-7949-43c1-a17b-70c2b1a2e0f4", null, "User", "USER" },
+                    { "f3dada1c-f273-4657-b857-67d0334bad7a", null, "Admin", "ADMIN" }
+                });
+        }
+    }
+}
