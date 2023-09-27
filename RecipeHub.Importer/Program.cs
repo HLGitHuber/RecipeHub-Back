d1c8c172572117326d99ee2c2882606b45e6521a
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-09-27 13:00:30 +0200

    feat: Working WebScrapping function

--- RecipeHub.Importer/Program.cs
+++ RecipeHub.Importer/Program.cs
@@ -1,10 +1,61 @@
-﻿namespace RecipeHub.Importer
+﻿using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using RecipeHub.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RecipeHub.Domain;
+
+namespace RecipeHub.Importer
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory) // Adjust the path as needed
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString = configuration.GetConnectionString("PGSQLDb");
+
+            var serviceProvider = new ServiceCollection()
+                .AddDbContext<RecipeDBContext>(options => options.UseNpgsql(connectionString))
+                .BuildServiceProvider();
+
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<RecipeDBContext>();
+                await ProcessIngredientsAsync(new HttpClient(), dbContext);
+            }
+
         }
+
+        static async Task ProcessIngredientsAsync(HttpClient client, RecipeDBContext dbContext)
+        {
+            var jsonResponse = await client.GetStringAsync(
+                "https://www.themealdb.com/api/json/v1/1/list.php?i=list");
+
+            List<IngredientData> ingredientDataResponse = JsonConvert.DeserializeObject<IngredientDataResponse>(jsonResponse).Meals;
+
+            foreach (var ingredientData in ingredientDataResponse)
+            {
+                var ingredient = new Ingredient()
+                {
+                    Name = ingredientData.strIngredient
+                };
+
+                dbContext.Ingredients.Add(ingredient);
+            }
+
+            await dbContext.SaveChangesAsync();
+        }
+
     }
 }
