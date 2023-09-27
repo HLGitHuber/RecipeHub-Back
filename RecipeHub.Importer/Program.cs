100585e163aad45aeb1841583d7956782398078f
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-09-27 11:49:51 +0200

    feat: added new project

---
+++ RecipeHub.Importer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RecipeHub.Importer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
