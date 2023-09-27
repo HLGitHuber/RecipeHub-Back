a2f1a91922e09ed3ef910a77f68ecd22eb580c64
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-09-27 12:59:59 +0200

    feat: added new class

---
+++ RecipeHub.Importer/IngredientDataResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeHub.Importer
+{
+    public class IngredientDataResponse
+    {
+        public List<IngredientData> Meals { get; set; }
+    }
+}
