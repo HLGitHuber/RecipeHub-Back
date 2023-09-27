4a125dbac147b2e2497b78272a4cf6e74fad9fd2
HLGitHuber <hubertlewandowski2002@gmail.com>
2023-09-27 11:50:02 +0200

    feat: added DataModel

---
+++ RecipeHub.Importer/IngredientData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeHub.Importer
+{
+    public class IngredientData
+    {
+        public string idIngredient { get; set; }
+        public string strIngredient { get; set; }
+    }
+}
