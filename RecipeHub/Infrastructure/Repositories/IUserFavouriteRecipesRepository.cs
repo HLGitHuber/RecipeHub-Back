a4afe997cec76edab44130457c391c46dea19fca
arkadiuszwoznica <arkadiusz.piotr.woznica@gmail.com>
2023-09-28 11:34:32 +0200

    user favourites repository created

---
+++ RecipeHub/Infrastructure/Repositories/IUserFavouriteRecipesRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using RecipeHub.Domain;
+
+namespace RecipeHub.Infrastructure.Repositories
+{
+    public interface IUserFavouriteRecipesRepository
+    {
+        Task<IEnumerable<Recipe>> GetUserFavouriteRecipesAsync(string userId);
+        Task<bool> AddRecipeToUserFavouritesAsync(string userId, int recipeId);
+        Task<bool> DeleteRecipeFromUserFavouritesAsync(string userId, int recipeId);
+    }
+}
+
